371e841efc71f4001eb091a4e1c997184c4e4397
Steven Gum <steven.gum@gmail.com>
2017-03-19 18:53:46 -0700

    minor fix

--- messages/dialogs/defaultDialog.js
+++ messages/dialogs/defaultDialog.js
@@ -47,6 +47,7 @@ exports.dialog = function() {
                 case 2:
                     session.send('At anytime you may type "exit" to reset the entire conversation.<br/>If you type "cancel" or "end", you can end your current dialog.');
                     builder.Prompts.confirm(session, 'Would you like to go back to the help menu? If not, this dialog will close.');
+                    break;
                 case 3:
                     session.endDialog('Okay! If you need any help with anything, type "help" at anytime for assistance.');
                     break;
